5a2b652cc772a94b0d5d59d2503125d32b465d8a
Cameron Aragon <caragon4695@gmail.com>
2021-02-04 13:15:18 -0700

    Fix App constructor

--- src/component/Poster.js
+++ src/component/Poster.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Poster.css';
 
-const Poster = ({title, rating, posterImage, id, key}) => {
+const Poster = ({title, rating, posterImage, id}) => {
     return (
         <div className='poster-wrapper'>
             <h2>{title}</h2>
             <p>{rating}</p>
-            <img id='{id}' src={posterImage} alt='Image of {title}'></img>
+            <img id={id} src={posterImage} alt={title}></img>
         </div>
     )
 }
