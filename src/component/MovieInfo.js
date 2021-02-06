7506948fb84a1a200478eb24ca22f0b0bb911d02
conconartist <conconart.info@gmail.com>
2021-02-05 17:39:02 -0700

    Add details to selected movie view

--- src/component/MovieInfo.js
+++ src/component/MovieInfo.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 import '../scss/index.scss';
+import '../scss/MovieInfo.scss';
 
 const MovieInfo = ({selectedMovie}) => {
     
     return (
-        <div className='selected-movie-display'>
-            <h2>{selectedMovie.title}</h2>
-        </div>
+        <section className='selected-movie-display'>
+            <button className="button-lobby">Back to Lobby</button>
+            <div className='movie-info-container'>
+              <h2 className='selected-movie-title'>{selectedMovie.title}</h2>
+              <img className='selected-movie-poster' src={selectedMovie.poster_path}></img>
+              <p className='selected-movie-overview'>{selectedMovie.overview}</p>
+              <p className='selected-movie-rating'>{selectedMovie.average_rating}</p>
+              <p className='selected-movie-date'>{selectedMovie.release_date}</p>
+              <p className='selected-movie-revenue'>{selectedMovie.revenue}</p>
+              <p className='selected-movie-runtime'>{selectedMovie.runtime}</p>
+              <p className='selected-movie-tagline'>{selectedMovie.tagline}</p>
+              <p className='selected-movie-genres'>{selectedMovie.genres.name}</p>
+            </div>  
+        </section>
     )
 }
 export default MovieInfo;
