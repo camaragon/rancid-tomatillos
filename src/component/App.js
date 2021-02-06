e396c2680a6b17761d2c51952b2ccb942845f67c
Cameron Aragon <caragon4695@gmail.com>
2021-02-05 19:27:37 -0700

    Add error handling to the main display

--- src/component/App.js
+++ src/component/App.js
@@ -26,7 +26,7 @@ class App extends Component {
       console.log(data.movies);
       this.setState({movies: data.movies, isFetching: false})
     }).catch(error => {
-      this.setState({isFetching: false})
+      this.setState({isFetching: false, error: error})
     });
   }
 
@@ -44,7 +44,6 @@ class App extends Component {
       this.setState({selectedMovie: data.movie, isLoading: false})
     }).catch(error => {
       console.log(error);
-      this.setState({isLoading: false, error: error})
     });
   }
 
@@ -54,6 +53,7 @@ class App extends Component {
         <header>
           <h1>Rancid Tomatillos</h1>
         </header>
+        {this.state.error && <h2>Uh oh! Looks like we can't find the movies!</h2>}
         {this.state.isFetching && <h2>The movies are on their way!</h2>}
         {this.state.selectedMovie && <MovieInfo isLoading={this.state.isLoading} selectedMovie={this.state.selectedMovie} handleChange={this.handleChange}/>}
         {!this.state.selectedMovie && <Movies movies={this.state.movies} selectMovie={this.selectMovie}/>}
