8b95668e4c4fb2bd431b1e1b695c5ed389061d0e
conconartist <conconart.info@gmail.com>
2021-02-07 11:58:27 -0700

    Add styling container for single movie display

--- src/component/MovieInfo.js
+++ src/component/MovieInfo.js
@@ -8,15 +8,17 @@ const MovieInfo = ({selectedMovie, handleClick}) => {
         <section className='selected-movie-display'>
             <button className="button-lobby" onClick={handleClick}>Back to Lobby</button>
             <div className='movie-info-container'>
-              <h2 className='selected-movie-title'>{selectedMovie.title}</h2>
               <img className='selected-movie-poster' src={selectedMovie.poster_path} alt={`Movie poster of ${selectedMovie.title}`} ></img>
-              <p className='selected-movie-overview'>{selectedMovie.overview}</p>
-              <p className='selected-movie-rating'>{selectedMovie.average_rating}</p>
-              <p className='selected-movie-date'>{selectedMovie.release_date}</p>
-              <p className='selected-movie-revenue'>{selectedMovie.revenue}</p>
-              <p className='selected-movie-runtime'>{selectedMovie.runtime}</p>
-              <p className='selected-movie-tagline'>{selectedMovie.tagline}</p>
-              <p className='selected-movie-genres'>{selectedMovie.genres}</p>
+              <div className='movie-info-container-inner'>
+                <h2 className='selected-movie-title'>{selectedMovie.title}</h2>
+                <p className='selected-movie-overview'>{selectedMovie.overview}</p>
+                <p className='selected-movie-rating'>{selectedMovie.average_rating}</p>
+                <p className='selected-movie-date'>{selectedMovie.release_date}</p>
+                <p className='selected-movie-revenue'>{selectedMovie.revenue}</p>
+                <p className='selected-movie-runtime'>{selectedMovie.runtime}</p>
+                <p className='selected-movie-tagline'>{selectedMovie.tagline}</p>
+                <p className='selected-movie-genres'>{selectedMovie.genres}</p>
+              </div>
             </div>  
         </section>
     )
