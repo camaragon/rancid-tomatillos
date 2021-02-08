7db3ad5126a5a6343936c130569ba894ee2a3ffe
conconartist <conconart.info@gmail.com>
2021-02-07 20:42:28 -0700

    Refactor layout and styling for single movie view

--- src/component/MovieInfo.js
+++ src/component/MovieInfo.js
@@ -23,15 +23,17 @@ const MovieInfo = ({selectedMovie, handleClick}) => {
             <div className='movie-info-container'>
               <img className='selected-movie-poster' src={selectedMovie.poster_path} alt={`Movie poster of ${selectedMovie.title}`} ></img>
               <div className='movie-info-container-inner'>
-                <h2 className='selected-movie-title'>{selectedMovie.title}</h2>
-                <p className='selected-movie-rating'>{selectedMovie.average_rating}/10 (Average Rating)</p>
-                <p className='selected-movie-date'>{selectedMovie.release_date}</p>
-                <div className='summary-container'>
+                <div className='selected-movie-header-container'>
+                  <h2 className='selected-movie-title'>{selectedMovie.title}</h2>
                   <p className='selected-movie-tagline'>{selectedMovie.tagline}</p>
+                </div>
+                <p className='selected-movie-rating'>{selectedMovie.average_rating}/10 (Average Rating)</p>
+                <div className='summary-container'>
                   <p className='selected-movie-overview'>{selectedMovie.overview}</p>
                   <p className='selected-movie-genres'>{selectedMovie.genres}</p>
                 </div>
-                <p className='selected-movie-revenue'>Revenue: {selectedMovie.revenue}</p>
+                <p className='selected-movie-revenue'>Revenue: ${selectedMovie.revenue}</p>
+                <p className='selected-movie-date'>{selectedMovie.release_date}</p>
                 <p className='selected-movie-runtime'>Runtime {selectedMovie.runtime} min</p>
               </div>
             </div>  
