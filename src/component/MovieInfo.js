1f6426b434a1d01ee957fdee73d207461c971ac8
conconartist <conconart.info@gmail.com>
2021-02-07 19:57:48 -0700

    Add containers for styling single movie view

--- src/component/MovieInfo.js
+++ src/component/MovieInfo.js
@@ -3,23 +3,39 @@ import '../scss/index.scss';
 import '../scss/MovieInfo.scss';
 
 const MovieInfo = ({selectedMovie, handleClick}) => {
-    
+    const roundedRating = {}
+    const dateConversion = {
+
+    }
+    const runTimeConversion = {
+
+    }
+    const spaceOutGenres = {
+
+    }
     return (
-        <section className='selected-movie-display'>
-            <button className="button-lobby" onClick={handleClick}>Back to Lobby</button>
+        <section className='selected-movie-display' style={{
+            backgroundImage: `url(${selectedMovie.backdrop_path})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+            }}>
             <div className='movie-info-container'>
               <img className='selected-movie-poster' src={selectedMovie.poster_path} alt={`Movie poster of ${selectedMovie.title}`} ></img>
               <div className='movie-info-container-inner'>
                 <h2 className='selected-movie-title'>{selectedMovie.title}</h2>
-                <p className='selected-movie-overview'>{selectedMovie.overview}</p>
-                <p className='selected-movie-rating'>{selectedMovie.average_rating}</p>
+                <p className='selected-movie-rating'>{selectedMovie.average_rating}/10 (Average Rating)</p>
                 <p className='selected-movie-date'>{selectedMovie.release_date}</p>
-                <p className='selected-movie-revenue'>{selectedMovie.revenue}</p>
-                <p className='selected-movie-runtime'>{selectedMovie.runtime}</p>
-                <p className='selected-movie-tagline'>{selectedMovie.tagline}</p>
-                <p className='selected-movie-genres'>{selectedMovie.genres}</p>
+                <div className='summary-container'>
+                  <p className='selected-movie-tagline'>{selectedMovie.tagline}</p>
+                  <p className='selected-movie-overview'>{selectedMovie.overview}</p>
+                  <p className='selected-movie-genres'>{selectedMovie.genres}</p>
+                </div>
+                <p className='selected-movie-revenue'>Revenue: {selectedMovie.revenue}</p>
+                <p className='selected-movie-runtime'>Runtime {selectedMovie.runtime} min</p>
               </div>
             </div>  
+            <button className='button-lobby' onClick={handleClick}>Back to Lobby</button>
         </section>
     )
 }
