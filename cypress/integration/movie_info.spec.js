680f716a2080aba5f4f2ee8bc727f18ace6156f3
conconartist <conconart.info@gmail.com>
2021-02-10 19:26:17 -0700

    Add tests to single movie display

--- cypress/integration/movie_info.spec.js
+++ cypress/integration/movie_info.spec.js
@@ -1,6 +1,37 @@
 describe('Movie Info Display', () => {
-    const baseUrl = 'http://localhost:3000/';
+    const movie1 = 'http://localhost:3000/337401';
+    const movie2 = 'http://localhost:3000/413518';
+    const movie3 = 'http://localhost:3000/579583';
     beforeEach(() => {
-        cy.visit(baseUrl);
+        cy.visit(movie1);
+        cy.visit(movie2);
+        cy.visit(movie3);
     });
+    it('should replace the background with an image from the selected movie', () => {
+
+    })
+    it('should display the movie poster', () => {
+
+    })
+    it('should display the movie title', () => {
+
+    })
+    it('should display an average rating', () => {
+
+    })
+    it('should display a summary of the movie', () => {
+
+    })
+    it('should display the movie\'s release date', () => {
+
+    })
+    it('should display the runtime of the selected movie', () => {
+
+    })
+    it('should display other details for the movie if available', () => {
+        //tagline, genres, revenue
+    })
+    it('should go back to the main page when the button is clicked', () => {
+
+    })
 })
