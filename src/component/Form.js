ea94791db463c1e1d6ac1ea42dc780aae9af0c02
Cameron Aragon <caragon4695@gmail.com>
2021-02-13 12:32:20 -0700

    Add a search bar on the main page

--- src/component/Form.js
+++ src/component/Form.js
@@ -4,7 +4,8 @@ import '../scss/Form.scss';
 const Form = () => {
     return (
         <form className='querying-form'>
-            <p>Search</p>
+            <label>Search</label>
+            <input type='text' placeholder='Search'></input>
             <div className='filter-sort'>
                 <p>Filter</p>
                 <p>Sort</p>
