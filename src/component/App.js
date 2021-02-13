923a27a022ef7b0f75fd81667f40c985f6dea4e6
Cameron Aragon <caragon4695@gmail.com>
2021-02-12 20:52:19 -0700

    Fixed the crashing from constant fetch calls

--- src/component/App.js
+++ src/component/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import '../scss/index.scss';
 import Movies from './Movies';
 import MovieInfo from './MovieInfo';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   constructor() {
@@ -13,7 +13,8 @@ class App extends Component {
       selectedMovie: {},
       isFetching: false, 
       isLoading: true,
-      error: false
+      error: false,
+      received: false
     } 
   }
 
@@ -23,7 +24,6 @@ class App extends Component {
     fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
     .then(response => response.json())
     .then(data => {
-      console.log(data.movies);
       this.setState({movies: data.movies, isFetching: false})
     }).catch(error => {
       this.state.error = true;
@@ -31,14 +31,16 @@ class App extends Component {
   }
 
   selectMovie = (id) => {   
-    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.movie);
-      this.setState({selectedMovie: data.movie})
-    }).catch(error => {
-      this.state.error = true;
-    }).finally({isLoading: false});
+    if (!this.state.received) {
+      fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.movie);
+        this.setState({selectedMovie: data.movie, received: true})
+      }).catch(error => {
+        this.state.error = true;
+      }).finally({isLoading: false});
+    }
   }
 
   render() {
@@ -49,43 +51,45 @@ class App extends Component {
         </header>
         {/* {this.state.isLoading && this.state.movies.length === 0 && <h2>Movie info is loading...</h2>} */}
         {this.state.isFetching && <h2 className='loading-text'>The movies are on their way!</h2>}
-        <Route 
-        exact 
-        path='/' 
-        render={ () => {
-        if (this.state.error && this.state.movies.length === 0) {
-          console.log(this.state.movies)
-          return (<h2 className='error-text'>Uh oh... we can't find the movies!</h2>)
-        }
-        return <Movies movies={this.state.movies} />
-      }}
-        />
-        <Route 
-        exact 
-        path='/:id' 
-        render={({ match }) => {
-          const currentMovie = this.state.movies.find(movie => movie.id === parseInt(match.params.id));
-          if (!currentMovie) {
-            return (<h2 className='error-text'>Uh oh... we can't find that movie!</h2>);
-          }
-          this.selectMovie(currentMovie.id);
-          return (
-          <MovieInfo 
-            match={match} 
-            title={this.state.selectedMovie.title}
-            backdrop={this.state.selectedMovie.backdrop_path}
-            poster={this.state.selectedMovie.poster_path}
-            overview={this.state.selectedMovie.overview}
-            rating={this.state.selectedMovie.average_rating}
-            date={this.state.selectedMovie.release_date}
-            revenue={this.state.selectedMovie.revenue}
-            runtime={this.state.selectedMovie.runtime}
-            tagline={this.state.selectedMovie.tagline}
-            genres={this.state.selectedMovie.genres} 
+        <Switch>
+          <Route 
+            exact 
+            path='/' 
+            render={ () => {
+            if (this.state.error && this.state.movies.length === 0) {
+              return (<h2 className='error-text'>Uh oh... we can't find the movies!</h2>)
+            }
+            this.state.received = false;
+            return <Movies movies={this.state.movies} />
+            }}
           />
-          )
-        }} 
-        />
+          <Route 
+            exact 
+            path='/:id' 
+            render={({ match }) => {
+              const currentMovie = this.state.movies.find(movie => movie.id === parseInt(match.params.id));
+              if (!currentMovie) {
+                return (<h2 className='error-text'>Uh oh... we can't find that movie!</h2>);
+              }
+              this.selectMovie(currentMovie.id);
+              return (
+              <MovieInfo 
+                match={match} 
+                title={this.state.selectedMovie.title}
+                backdrop={this.state.selectedMovie.backdrop_path}
+                poster={this.state.selectedMovie.poster_path}
+                overview={this.state.selectedMovie.overview}
+                rating={this.state.selectedMovie.average_rating}
+                date={this.state.selectedMovie.release_date}
+                revenue={this.state.selectedMovie.revenue}
+                runtime={this.state.selectedMovie.runtime}
+                tagline={this.state.selectedMovie.tagline}
+                genres={this.state.selectedMovie.genres} 
+              />
+              )
+            }} 
+          />
+        </Switch>
 
       </main>
     );
