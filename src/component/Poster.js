b50657a141abd4d9ea3c9ccc0fcbe6d21edd4304
Cameron Aragon <caragon4695@gmail.com>
2021-02-16 15:14:39 -0700

    Refactor the Poster component

--- src/component/Poster.js
+++ src/component/Poster.js
@@ -10,7 +10,14 @@ const Poster = ({title, rating, posterImage, id}) => {
               <p className='poster-rating'>{`${rating}`}/10</p>
               <Tomato />
             </div>
-            <Link to={`/${id}`}> <img className='poster-image' id={id} src={posterImage} alt={`Movie poster of ${title}`}></img></Link> 
+            <Link to={`/${id}`}> 
+                <img 
+                    className='poster-image' 
+                    id={id} 
+                    src={posterImage} 
+                    alt={`Movie poster of ${title}`}>
+                </img>
+            </Link> 
             <h2 className='poster-title'>{title}</h2>
         </div>
     )
