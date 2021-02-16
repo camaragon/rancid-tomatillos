e121141602c7c971e15731464b057b32caed1689
Cameron Aragon <caragon4695@gmail.com>
2021-02-16 15:33:11 -0700

    Delete unnecesary files

--- cypress/integration/movie_display.spec.js
+++ cypress/integration/movie_display.spec.js
@@ -1,13 +1,16 @@
 describe('Main Display', () => {
     const baseUrl = 'http://localhost:3000';
+    
     it('should be able to visit the base url and see a grid display of movie posters', () => {
         cy
         .visit(baseUrl)
         .location()
     });
+
     beforeEach(() => {
         cy.visit(baseUrl);
     });
+
     it('should display a content loading screen while the posters are loading to inform the user', () => {
         cy
         .get('.content-loader')
